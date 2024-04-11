6483ed04f88e639b69899cdee6ea4bd91a6a879d
Bob Recio <bobrecio@gmail.com>
2024-04-11 13:52:01 -0500

    finished course

--- 10.SendingEmail/send_email.py
+++ 10.SendingEmail/send_email.py
@@ -7,6 +7,7 @@
 import requests
 import json
 import smtplib
+import logging
 from email.mime.text import MIMEText
 
 url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
@@ -18,12 +19,20 @@ with open(CREDS_FILE, "r") as f:
     creds = json.load(f)
     f.close
 
+LOGFILE = "email_send.log"
 EMAIL = creds["email"]
 PASSWORD = creds["password"]
-FROM = EMAIL
-RECIPIENT = EMAIL
 SMTP = creds["smtp"]
 PORT = creds["port"]
+FROM = EMAIL
+RECIPIENT = EMAIL
+
+logging.basicConfig(
+    filemode="a",
+    filename=LOGFILE,
+    level=logging.INFO,
+    format="%(levelname)s - %(asctime)s - %(message)s",
+)
 
 
 def send_email(joke):
@@ -34,11 +43,28 @@ def send_email(joke):
     msg["To"] = RECIPIENT
     # ---
 
-    s = smtplib.SMTP_SSL(SMTP, PORT)  # use SMTP_SSL
-    # s.starttls() # don't use this
-    s.login(EMAIL, PASSWORD)
-    s.sendmail(FROM, RECIPIENT, msg.as_string())
+    try:
+        s = smtplib.SMTP_SSL(SMTP, PORT)  # use SMTP_SSL
+    except smtplib.SMTPConnectError:
+        logging.error("Could not connect to mail server.")
+        exit()
+
+    try:
+        s.login(EMAIL, PASSWORD)
+    except smtplib.SMTPAuthenticationError:
+        logging.error(
+            f"Login failed. Did you set the password in the creds file ({CREDS_FILE})"
+        )
+        exit()
+
+    try:
+        s.sendmail(FROM, RECIPIENT, msg.as_string())
+    except smtplib.SMTPException:
+        logging.error("Could not send email due to error")
+        exit()
+
     s.quit()
+
     print(f"---\n{msg.as_string()}")
 
 
@@ -46,6 +72,7 @@ def get_joke_content():
     response = requests.get(url)
     result = response.json()
     if result["error"]:
+        logging.error("Something went wrong with the API request")
         return None
     if result["type"] == "twopart":
         setup = result["setup"]
@@ -57,6 +84,7 @@ def get_joke_content():
 
 
 joke = get_joke_content()
-
+logging.info("Joke retrieved from API")
 if joke is not None:
     send_email(joke)
+    logging.info("Joke sent")
